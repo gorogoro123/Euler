63f22674578ca10c9ac689cc23c5df858da1e144
gorogoro123 <gorogoro123@gmail.com>
2013-10-21 15:47:46 +0900

    add

---
+++ 010.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+max = 2000000
+
+array = Array.new()
+array.push(2)
+
+for i in 3 .. max
+	i_sq = Math.sqrt(i).to_i
+	for j in 0 .. array.size - 1
+		if i % array[j] == 0
+			break
+		elsif i_sq < array[j]
+			array.push(i)
+#			puts i
+			break
+		end
+	end
+end
+
+sum = 0
+for i in 0 .. array.size - 1
+	sum += array[i]
+end
+
+printf("sum = %d\n", sum );
