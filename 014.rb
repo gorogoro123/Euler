75f8aa306b6a91e010b96545b555b7e322188ba3
gorogoro123 <gorogoro123@gmail.com>
2013-10-22 16:27:42 +0900

    mod

--- 014.rb
+++ 014.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/ruby
 
+max = 1000000
+ary = Array.new(max+1, 0)
+
 i_max = 0
 cnt_max = 0
-for i in 0 .. 1000000
+for i in 0 .. max
 	val = i
 	cnt = 0
 	while val > 1
+		#check stored data
+		if val < ary.size and ary[val] > 0
+			break
+		end
+
 		if (val % 2) == 0
 			val = val / 2
 		else
@@ -16,6 +24,9 @@ for i in 0 .. 1000000
 	end
 
 #	puts cnt
+
+	cnt += ary[val]
+	ary[i] = cnt
 
 	if cnt_max < cnt
 		i_max = i
